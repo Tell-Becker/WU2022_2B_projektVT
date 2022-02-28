95fdf438b216e298567dab4337f3578b1fdd2b8d
Tell-Becker <83348335+Tell-Becker@users.noreply.github.com>
2022-02-28 14:50:04 +0100

    hamburgaremeny
    Har arbetat med att skapa en fungerande hamburgaremeny till min mobillayout. Layouten går ner lodrätt. Därefter har kruk.html arbetats på samt har css till html dokumentet gjorts.

---
+++ js/script.js
@@ -0,0 +1,9 @@
+const burger = document.querySelector('#hamburgare_meny')
+const nav = document.querySelector('#meny_lista')
+
+function toggleMenu() {
+    nav.classList.toggle('meny')
+    burger.classList.toggle('toggle')
+}
+
+burger.addEventListener('click', toggleMenu) 
